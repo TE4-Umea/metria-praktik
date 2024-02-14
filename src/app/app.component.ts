d1d4aaab09cf4d2602c5efa4f8ee2386aab6811c
timnilsson04 <timnils04@gmail.com>
2024-02-14 08:55:58 +0100

    🎉 The Beginning of the End

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MapComponent } from './map/map.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MapComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/map/map.component.ts
@@ -0,0 +1,52 @@
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import { fromLonLat, toLonLat } from 'ol/proj';
+import OSM from 'ol/source/OSM';
+
+@Component({
+  selector: 'app-map',
+  standalone: true,
+  imports: [],
+  templateUrl: './map.component.html',
+  styleUrl: './map.component.scss'
+})
+export class MapComponent implements OnInit, OnDestroy {
+
+  @ViewChild('mapElement', { static: true }) mapElement: ElementRef | undefined;
+
+  map: Map | undefined;
+
+  ngOnInit(): void {
+    this.initMap();
+  }
+
+  ngOnDestroy(): void {
+    this.map?.setTarget(undefined);
+  }
+
+  private initMap(): void {
+    this.map = new Map({
+      target: this.mapElement?.nativeElement,
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        })
+      ],
+      view: new View({
+        center: fromLonLat([20.242829157757257, 63.82811461193097]),
+        zoom: 11
+      })
+    });
+
+    this.map.on('singleclick', this.handleMapClick.bind(this))
+  }
+
+  private handleMapClick(event: any): void {
+    console.log(`Zoom level: ${this.map?.getView().getZoom()}`);
+    console.log(`Map coordinates (wgs84): ${toLonLat(event.coordinate)}`);
+    console.log(`Pixel coordinates (top-left): ${event.pixel}`);
+  }
+}
+
