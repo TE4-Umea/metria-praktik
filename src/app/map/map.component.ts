6c5f24a45d5592ca71c9c92bd640428529297338
timnilsson04 <timnils04@gmail.com>
2024-02-19 10:11:25 +0100

    Add user interface dropdown menu

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,3 +1,4 @@
+import { UserInterfaceComponent } from '../user-interface/user-interface.component'
 import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core'
 import Map from 'ol/Map'
 import View from 'ol/View'
@@ -14,75 +15,75 @@ import { click } from 'ol/events/condition'
 @Component({
     selector: 'app-map',
     standalone: true,
-    imports: [],
+    imports: [UserInterfaceComponent],
     templateUrl: './map.component.html',
     styleUrl: './map.component.scss'
 })
 export class MapComponent implements OnInit, OnDestroy {
 
-  @ViewChild('mapElement', { static: true }) mapElement: ElementRef | undefined
+    @ViewChild('mapElement', { static: true }) mapElement: ElementRef | undefined
 
-  map: Map | undefined
+    map: Map | undefined
 
-  ngOnInit(): void {
-      this.initMap()
-  }
+    ngOnInit(): void {
+        this.initMap()
+    }
 
-  ngOnDestroy(): void {
-      this.map?.setTarget(undefined)
-  }
+    ngOnDestroy(): void {
+        this.map?.setTarget(undefined)
+    }
 
-  private initMap(): void {
-      this.map = new Map({
-          target: this.mapElement?.nativeElement,
-          layers: [
-              new TileLayer({
-                  source: new OSM()
-              }),
-              new VectorLayer({
-                  source: new VectorSource({
-                      url: 'assets/sweden.json',
-                      format: new GeoJSON()
-                  })
-              })
-          ],
-          view: new View({
-              center: fromLonLat([20.242829157757257, 63.82811461193097]),
-              zoom: 11
-          })
-      })
+    private initMap(): void {
+        this.map = new Map({
+            target: this.mapElement?.nativeElement,
+            layers: [
+                new TileLayer({
+                    source: new OSM()
+                }),
+                new VectorLayer({
+                    source: new VectorSource({
+                        url: 'assets/sweden.json',
+                        format: new GeoJSON()
+                    })
+                })
+            ],
+            view: new View({
+                center: fromLonLat([20.242829157757257, 63.82811461193097]),
+                zoom: 11
+            })
+        })
 
-      this.map.on('singleclick', this.handleMapClick.bind(this))
-  }
+        this.map.on('singleclick', this.handleMapClick.bind(this))
+    }
 
-  private handleMapClick(event: any): void {
-      let hasSelected = false
-      const selectedStyle = new Style({
-          fill: new Fill({
-              color: 'rgba(0, 106, 167, 0.3)',
-          }),
-          stroke: new Stroke({
-              color: 'rgba(0, 106, 167, 0.7)',
-              width: 2,
-          }),
-      })
+    private handleMapClick(event: any): void {
+        let hasSelected: boolean = false
+        const selectedStyle = new Style({
+            fill: new Fill({
+                color: 'rgba(0, 106, 167, 0.3)',
+            }),
+            stroke: new Stroke({
+                color: 'rgba(0, 106, 167, 0.7)',
+                width: 2,
+            }),
+        })
 
-      const mapReference: Map | undefined = this.map
-      this.map?.on('click', function (event) {
-          if (hasSelected === false) {
-              hasSelected = true
-              const select = new Select({ condition: click, style: selectedStyle })
-              mapReference?.addInteraction(select)
-              mapReference?.forEachFeatureAtPixel(event.pixel, function (feature) {
-                  console.log(feature.get('name'))
-              })
-          }
-      })
+        const mapReference: Map | undefined = this.map
+        this.map?.on('click', function (event) {
+            if (hasSelected === false) {
+                hasSelected = true
+                const select = new Select({ condition: click, style: selectedStyle })
+                mapReference?.addInteraction(select)
+                mapReference?.forEachFeatureAtPixel(event.pixel, function (feature) {
+                    console.log(feature.get('name'))
+                })
+            }
+        })
 
 
-      console.log(`Zoom level: ${this.map?.getView().getZoom()}`)
-      console.log(`Map coordinates (wgs84): ${toLonLat(event.coordinate)}`)
-      console.log(`Pixel coordinates (top-left): ${event.pixel}`)
-  }
+        console.log(`Zoom level: ${this.map?.getView().getZoom()}`)
+        console.log(`Map coordinates (wgs84): ${toLonLat(event.coordinate)}`)
+        console.log(`Pixel coordinates (top-left): ${event.pixel}`)
+    }
 }
 
