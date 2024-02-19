865252602147d2edab6940745f42311d4d8860ea
timnilsson04 <timnils04@gmail.com>
2024-02-19 09:09:03 +0100

    eslint

---
+++ src/app/user-interface/user-interface.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'app-user-interface',
+    standalone: true,
+    imports: [],
+    templateUrl: './user-interface.component.html',
+    styleUrl: './user-interface.component.scss'
+})
+export class UserInterfaceComponent {
+
+}
