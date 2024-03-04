eeb6c2b57f96026981f3a2e78209b899367901a7
Albert <albert.sparrman@gmail.com>
2024-03-04 15:23:14 +0100

    karusell haha kom ihåg att npm i

--- src/app/user-interface/user-interface.component.ts
+++ src/app/user-interface/user-interface.component.ts
@@ -1,17 +1,22 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, ViewChild } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { MapComponent } from '../map/map.component'
 import { MatSlideToggleModule } from '@angular/material/slide-toggle'
 import { SetShowBuildings } from '../service'
+import { DragScrollComponent, DragScrollItemDirective } from 'ngx-drag-scroll'
+
 @Component({
     selector: 'app-user-interface',
     standalone: true,
-    imports: [CommonModule, MapComponent, MatSlideToggleModule],
+    imports: [CommonModule, MapComponent, MatSlideToggleModule, DragScrollComponent, DragScrollItemDirective],
     templateUrl: './user-interface.component.html',
     styleUrl: './user-interface.component.scss'
 })
 export class UserInterfaceComponent implements OnInit {
     constructor(private setShowBuildings: SetShowBuildings) { }
+
+    @ViewChild('carousel', { read: DragScrollComponent }) ds!: DragScrollComponent
+
 
     resources: { [resource: string]: number } = { Money: 0, Stone: 0, Wood: 0, Metal: 0, Food: 0, Electricity: 0, Oil: 0, People: 0, Weapons: 0 }
 
@@ -35,4 +40,11 @@ export class UserInterfaceComponent implements OnInit {
         this.showMenu = !this.showMenu
     }
 
+    carouselLeft(): void {
+        this.ds.moveLeft()
+    }
+
+    carouselRight(): void {
+        this.ds.moveRight()
+    }
 }
