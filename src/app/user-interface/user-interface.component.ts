39956421839460b23a08dff2aff4b4908bd95f32
timnilsson04 <timnils04@gmail.com>
2024-03-20 13:29:36 +0100

    Update user-interface component with menu item and player names

--- src/app/user-interface/user-interface.component.ts
+++ src/app/user-interface/user-interface.component.ts
@@ -1,13 +1,15 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Component, OnInit, ViewChild } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { MapComponent } from '../map/map.component'
 import { MatSlideToggleModule } from '@angular/material/slide-toggle'
-import { SetShowBuildings } from '../service'
+import { Decoder, GetCookie, SetShowBuildings } from '../service'
 import { DragScrollComponent, DragScrollItemDirective } from 'ngx-drag-scroll'
 
 import { Lobby } from '../http.service'
 import { interval } from 'rxjs'
 import { MatButtonModule } from '@angular/material/button'
+import { Router } from '@angular/router'
 
 @Component({
     selector: 'app-user-interface',
@@ -17,12 +19,12 @@ import { MatButtonModule } from '@angular/material/button'
     styleUrl: './user-interface.component.scss'
 })
 export class UserInterfaceComponent implements OnInit {
-    constructor(private setShowBuildings: SetShowBuildings, private lobby: Lobby) { }
+    constructor(private setShowBuildings: SetShowBuildings, public router: Router, private decoder: Decoder, private getCookie: GetCookie, private lobby: Lobby) { }
 
     @ViewChild('carousel', { read: DragScrollComponent }) ds!: DragScrollComponent
 
 
-    resources: { [resource: string]: number } = { Money: 0, Stone: 0, Wood: 0, Metal: 0, Food: 0, Electricity: 0, Oil: 0, People: 0, Weapons: 0 }
+    resources: { [resource: string]: number } = { Money: 0, BuildingMaterials: 0, Army: 0 }
 
     information: { [info: string]: string | number } = { Weather: 'Sunny', Date: '2021-01-01', Round: 1, Level: 1 }
 
@@ -30,7 +32,10 @@ export class UserInterfaceComponent implements OnInit {
     showMenu: boolean = false
     showBuildings: boolean = true
 
-    player2Active: boolean = false 
+    playerName: string = ''
+    enemyPlayerNames: string[] = []
+
+    player2Active: boolean = false
 
     testPlayerSizing() {
         this.player2Active = !this.player2Active
@@ -38,8 +43,12 @@ export class UserInterfaceComponent implements OnInit {
 
 
     ngOnInit() {
-        this.getLobby()
+        this.getLobbyNames()
+        this.getLobbyData()
         this.toggleBuildings()
+        interval(10000).subscribe(() => {
+            this.getLobbyData()
+        })
     }
 
 
@@ -49,18 +58,31 @@ export class UserInterfaceComponent implements OnInit {
         })
     }
 
-    getLobby() {
-        interval(30000).subscribe(() => {
-            this.lobby.getLobby().subscribe((data) => {
-                console.log(data)
+    getLobbyData() {
+        this.lobby.getLobby().subscribe(() => {
+
+        })
+    }
+
+    getLobbyNames() {
+        this.playerName = this.decoder.decoder(this.getCookie.getCookie('token') || '').user_information.username
+        this.lobby.getLobby().subscribe((data: any) => {
+            if (data.lobbyOwner !== this.playerName) {
+                this.enemyPlayerNames.push(data.lobbyOwner)
+            }
+            data.players.forEach((element: any) => {
+                if (element.username !== this.playerName) {
+                    this.enemyPlayerNames.push(element.username)
+                }
             })
         })
     }
 
-    // onClickPutLobbyData() {
-    //     const data: { [resource: string]: number } = this.resources
-    //     this.lobby.putLobbyData(data)
-    // }
+
+    onClickPutLobbyData() {
+        const data: { [resource: string]: number } = this.resources
+        this.lobby.putLobbyData(data)
+    }
 
 
     carouselLeft(): void {
