c7c015a7ee9d7be84e387556914c8de663f89868
timnilsson04 <timnils04@gmail.com>
2024-03-20 14:32:59 +0100

    Add start-game.html and lan-choose.html templates, and update map.component.ts

--- src/app/user-interface/user-interface.component.ts
+++ src/app/user-interface/user-interface.component.ts
@@ -10,6 +10,12 @@ import { Lobby } from '../http.service'
 import { interval } from 'rxjs'
 import { MatButtonModule } from '@angular/material/button'
 import { Router } from '@angular/router'
+import { HttpClientModule } from '@angular/common/http'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { MatDialogActions, MatDialogClose, MatDialogTitle, MatDialogContent, MatDialogRef, MatDialog } from '@angular/material/dialog'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatIconModule } from '@angular/material/icon'
+import { MatInputModule } from '@angular/material/input'
 
 @Component({
     selector: 'app-user-interface',
@@ -17,9 +23,10 @@ import { Router } from '@angular/router'
     imports: [CommonModule, MapComponent, MatSlideToggleModule, DragScrollComponent, DragScrollItemDirective, MatButtonModule],
     templateUrl: './user-interface.component.html',
     styleUrl: './user-interface.component.scss'
+
 })
 export class UserInterfaceComponent implements OnInit {
-    constructor(private setShowBuildings: SetShowBuildings, public router: Router, private decoder: Decoder, private getCookie: GetCookie, private lobby: Lobby) { }
+    constructor(public dialog: MatDialog, private setShowBuildings: SetShowBuildings, public router: Router, private decoder: Decoder, private getCookie: GetCookie, private lobby: Lobby) { }
 
     @ViewChild('carousel', { read: DragScrollComponent }) ds!: DragScrollComponent
 
@@ -35,6 +42,9 @@ export class UserInterfaceComponent implements OnInit {
     playerName: string = ''
     enemyPlayerNames: string[] = []
 
+    lobbyOwnerChosen: boolean = false
+    lanChosen: boolean = false
+
     player2Active: boolean = false
 
     testPlayerSizing() {
@@ -45,16 +55,25 @@ export class UserInterfaceComponent implements OnInit {
     ngOnInit() {
         this.getLobbyNames()
         this.getLobbyData()
-        this.toggleBuildings()
+        this.toggleBuildings('250ms', '250ms')
         interval(10000).subscribe(() => {
             this.getLobbyData()
         })
     }
 
 
-    toggleBuildings(): void {
+
+
+    toggleBuildings(enterAnimationDuration: string, exitAnimationDuration: string): void {
         this.setShowBuildings.showBuildings$.subscribe(show => {
             this.showBuildings = show
+            if (this.showBuildings === true) {
+                this.dialog.open(LanChoose, {
+                    width: '380px',
+                    enterAnimationDuration,
+                    exitAnimationDuration,
+                })
+            }
         })
     }
 
@@ -78,6 +97,22 @@ export class UserInterfaceComponent implements OnInit {
         })
     }
 
+    checkLanChoose(enterAnimationDuration: string, exitAnimationDuration: string): void {
+        this.lobby.getLobby().subscribe((data: any) => {
+            if (this.lanChosen === true && this.lobbyOwnerChosen === true && this.decoder.decoder(this.getCookie.getCookie('token') || '').user_information.username === data.lobbyOwner) {
+                this.dialog.open(StartGame, {
+                    width: '380px',
+                    enterAnimationDuration,
+                    exitAnimationDuration,
+                })
+            }
+            data.players.forEach((element: any) => {
+                if (element.status === 'chosen') {
+                    this.lanChosen = true
+                }
+            })
+        })
+    }
 
     onClickPutLobbyData() {
         const data: { [resource: string]: number } = this.resources
@@ -91,6 +126,39 @@ export class UserInterfaceComponent implements OnInit {
 
     carouselRight(): void {
         this.ds.moveRight()
+    }
+
+}
+
+@Component({
+    selector: 'lan-choose',
+    templateUrl: 'lan-choose.html',
+    styleUrl: '../starting-screen/starting-screen.component.scss',
+    standalone: true,
+    imports: [CommonModule, MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule, MatDialogActions, MatDialogClose, MatDialogTitle, MatDialogContent, FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, HttpClientModule],
+    providers: []
+})
+export class LanChoose {
+    constructor(public dialogRef: MatDialogRef<LanChoose>, private lobby: Lobby, private decoder: Decoder, private getCookie: GetCookie) { }
+
+    chosenLan() {
+        this.lobby.putLobbyPlayers([{ status: 'chosen', username: this.decoder.decoder(this.getCookie.getCookie('token') || '').user_information.username }])
+    }
+
+}
+
+@Component({
+    selector: 'start-game',
+    templateUrl: 'start-game.html',
+    styleUrl: '../starting-screen/starting-screen.component.scss',
+    standalone: true,
+    imports: [CommonModule, MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule, MatDialogActions, MatDialogClose, MatDialogTitle, MatDialogContent, FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, HttpClientModule],
+    providers: []
+})
+export class StartGame {
+    constructor(public dialogRef: MatDialogRef<StartGame>, private lobby: Lobby, private decoder: Decoder, private getCookie: GetCookie) { }
+
+    startGame() {
     }
 
 }
